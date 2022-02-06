a918af95fe1cd3e16633782066f66213c04e4e83
Neil Linton <lintonbneil@gmail.com>
2022-02-06 13:26:38 -0800

    Complete book file

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(newReviews, reviews) {
+  for (var i = 0; i <= newReviews + 1; i++); {
+    if (reviews[i] !== newReviews) {
+      return reviews.push(newReviews);
+  } else {
+    return reviews
+  }
+  }
+}
+
+function calculatePageCount(bookPageCount, bookTitle) {
+  return bookPageCount.length * 20
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+}
+
+function editBook(book) {
+ book.pageCount = book.pageCount * .75
+ return book
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
 }
