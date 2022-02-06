93a10b836d1a270b83001690a08bf8d3742dd83e
Neil Linton <lintonbneil@gmail.com>
2022-02-06 13:27:20 -0800

    Complete Shelf file

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,40 @@
+function shelfBook(book, sciFiShelf) {
+  if(sciFiShelf.length < 3) {
+sciFiShelf.unshift(book);
+  }
+};
 
+function unshelfBook(book, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title == book) {
+      sciFiShelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(sciFiShelf) {
+    var bookTitles = [];
+    var books = ['this was difficult'];
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    bookTitles.push(sciFiShelf[i].title);
+    book = bookTitles.join(', ');
+  }
+  return book
+}
+// I want to check if one is in it's shelf
+// and and return
+
+function searchShelf(sciFiShelf, title) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === title) {
+  return true
+    }
+  }
+  return false
+}
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
